621bfceab31c786e77480fb0948350d9d0ae86ff
Srihitha Reddy <srihitha@email.com>
2026-02-27 11:20:53 +0530

    UC7 completed

--- App/src/UC7.java
+++ App/src/UC7.java
@@ -1,24 +1,20 @@
-import java.util.Stack;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
-public class UC6 {
+public class UC7 {
     public static void main(String[] args) {
-        String input = "MADAM";
+        String input = "LEVEL";
 
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);
-            queue.add(ch);
-        }
-
-        while (!stack.isEmpty()) {
-            if (!stack.pop().equals(queue.remove())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
